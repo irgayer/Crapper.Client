4cae88f65aaf0864071fa8b47b1b02c1c167bbc5
Yerbol Irgaliyev <yerbol.irgaliyev@gmail.com>
2022-06-07 21:07:40 +0600

    add get following posts

--- src/views/Following.svelte
+++ src/views/Following.svelte
@@ -1,7 +1,21 @@
 <script>
-
 import PostList from "../components/PostList.svelte";
+
+import {onMount} from "svelte";
+import axios from "axios";
+import {apiAuth, apiHost} from "../config/config";
+import {jwtToken} from "../config/stores";
+
+let posts = [];
+
+onMount(async () => {
+    axios.get(apiHost + '/posts/subs', apiAuth($jwtToken))
+        .then(data => {
+            posts = data.data;
+        })
+        .catch(e => console.log(e.error))
+})
 </script>
 
 <h1>Following</h1>
-<PostList posts="{[]}"/>
+<PostList posts="{posts}"/>
